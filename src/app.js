871e9b8a3468ea14225186ddb677932e58534f4b
Dushyant <dushyantbhalgami@gmail.com>
2018-11-28 21:47:28 +0530

    changes for ecs

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@
  * This is the top-level module of the application.
  */
 
+console.log('**********')
+console.log(process.env.LD_PRELOAD)
+console.log(process.env.LLVM_INSTALL_PREFIX)
+console.log('**********')
+
 global.Promise = require('bluebird')
 const healthcheck = require('topcoder-healthcheck-dropin')
 const listener = require('./listener')
