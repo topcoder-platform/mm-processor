4e425f2f6751f5786a07c681e96ec3b0e36d7212
Dushyant <dushyantbhalgami@gmail.com>
2018-11-28 23:24:54 +0530

    changes for ecs

--- env.sh
+++ env.sh
@@ -9,9 +9,9 @@ if [ x"$UNAME" = x"Darwin" ]; then
   export DYLD_INSERT_LIBRARIES="$HOME/.cpp-mm-scoring/cling-0.5/lib/libcling.dylib"
   export PKG_CONFIG_PATH=/Library/Frameworks/Mono.framework/Versions/Current/lib/pkgconfig
 else
-  export LD_PRELOAD=$LD_PRELOAD:"$HOME/.cpp-mm-scoring/cling-0.5/lib/libcling.so"
+  export LD_PRELOAD="/mm-processor/node_modules/cpp-mm-scoring/.sources/cling-0.5/lib/libcling.so"
 fi
-export CLING_DIR="$HOME/.cpp-mm-scoring/cling-0.5/lib/cmake/cling"
-export LLVM_INSTALL_PREFIX="$HOME/.cpp-mm-scoring/cling-0.5"
-export CLING_LIB_DIR="$HOME/.cpp-mm-scoring/cling-0.5/lib"
-export NLOHMANN_JSON_INCLUDE_DIR="$HOME/.cpp-mm-scoring/include"
+export CLING_DIR="/mm-processor/node_modules/cpp-mm-scoring/.sources/cling-0.5/lib/cmake/cling"
+export LLVM_INSTALL_PREFIX="/mm-processor/node_modules/cpp-mm-scoring/.sources/cling-0.5"
+export CLING_LIB_DIR="/mm-processor/node_modules/cpp-mm-scoring/.sources/cling-0.5/lib"
+export NLOHMANN_JSON_INCLUDE_DIR="/mm-processor/node_modules/cpp-mm-scoring/.sources/include"
