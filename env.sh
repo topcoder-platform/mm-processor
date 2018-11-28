3047c335a41095ff443ebfe3a0ae99996603f0ae
Dushyant <dushyantbhalgami@gmail.com>
2018-11-28 12:51:46 +0530

    changes for ecs

--- env.sh
+++ env.sh
@@ -15,9 +15,3 @@ export CLING_DIR="$HOME/.cpp-mm-scoring/cling-0.5/lib/cmake/cling"
 export LLVM_INSTALL_PREFIX="$HOME/.cpp-mm-scoring/cling-0.5"
 export CLING_LIB_DIR="$HOME/.cpp-mm-scoring/cling-0.5/lib"
 export NLOHMANN_JSON_INCLUDE_DIR="$HOME/.cpp-mm-scoring/include"
-
-# kafka
-export CONNECTION_STRING=kafka-host:9092
-# aws
-export ACCESS_KEY_ID=${ACCESS_KEY_ID:-update}
-export SECRET_ACCESS_KEY=${SECRET_ACCESS_KEY:-update}
